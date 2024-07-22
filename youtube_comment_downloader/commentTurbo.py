284fb0f57cd7ed05e544ed34c11cd09e08c6b3ee
shaburu <xshaburu@gmail.com>
2024-07-22 15:08:37 +0530

    multi improvement

--- youtube_comment_downloader/commentTurbo.py
+++ youtube_comment_downloader/commentTurbo.py
@@ -1,14 +1,53 @@
-from youtube_comment_downloader import *
-downloader = YoutubeCommentDownloader()
-comm_arr = ''
-comment_count = 0
-comments = downloader.get_comments_from_url('https://www.youtube.com/watch?v=_9b1G0Ivaas')
-for comment in comments:
-    comm_arr += comment['text'] + '; '
-    print(comment['cid'])
-    comment_count += 1
+from youtube_comment_downloader import YoutubeCommentDownloader
+from multiprocessing import Process, Manager
+import time
 
-print(comment_count)
+def download_comments(url, start, end, return_dict, index):
+    downloader = YoutubeCommentDownloader()
+    comments = downloader.get_comments_from_url(url)
+    comm_arr = ''
+    comment_count = 0
+    for i, comment in enumerate(comments):
+        if i < start:
+            continue
+        if i >= end:
+            break
+        comm_arr += comment['text'] + '; '
+        comment_count += 1
+    return_dict[index] = (comm_arr, comment_count)
 
-with open('temp2.txt', 'w', encoding='utf-8') as f:
-    f.write(comm_arr)
+if __name__ == "__main__":
+    url = 'https://www.youtube.com/watch?v=G8AT01tuyrk'
+    num_processes = 4 # Number of processes you want to run
+    total_comments_to_fetch = 1005 # Total number of comments to fetch
+    comments_per_process = total_comments_to_fetch // num_processes
+
+    manager = Manager()
+    return_dict = manager.dict()
+    
+    processes = []
+
+    start_time = time.time()
+
+    for i in range(num_processes):
+        start = i * comments_per_process
+        end = (i + 1) * comments_per_process
+        proc = Process(target=download_comments, args=(url, start, end, return_dict, i))
+        processes.append(proc)
+        proc.start()
+
+    for proc in processes:
+        proc.join()
+
+    combined_comments = ''
+    total_comment_count = 0
+    for i in range(num_processes):
+        combined_comments += return_dict[i][0]
+        total_comment_count += return_dict[i][1]
+
+    with open('temp2.txt', 'w', encoding='utf-8') as f:
+        f.write(combined_comments)
+
+    end_time = time.time()
+    print(f'Total comments fetched: {total_comment_count}')
+    print(f'Time taken: {end_time - start_time} seconds')
--- youtube_comment_downloader/commenttojson.py
+++ youtube_comment_downloader/commenttojson.py
@@ -1,15 +1,20 @@
-from itertools import islice
 from youtube_comment_downloader import *
 downloader = YoutubeCommentDownloader()
+import time
+
 comm_arr = ''
 comment_count = 0
-comments = downloader.get_comments_from_url('https://www.youtube.com/watch?v=_9b1G0Ivaas')
+comments = downloader.get_comments_from_url('https://www.youtube.com/watch?v=G8AT01tuyrk')
 for comment in comments:
+    start_time = time.time()
     comm_arr += comment['text'] + '; '
     print(comment['cid'])
     comment_count += 1
 
+end_time = time.time()
+
 print(comment_count)
+print(f'Time taken: {end_time - start_time} seconds')
 
 with open('temp2.txt', 'w', encoding='utf-8') as f:
     f.write(comm_arr)
